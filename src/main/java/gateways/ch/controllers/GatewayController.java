682638335f6c69b561310ce0deb2ea5f09670d5d
Francisco R Espinosa <franciscoreg9620@gmail.com>
2021-11-27 10:32:01 -0500

    Last Commit: All ok

--- src/main/java/gateways/ch/controllers/GatewayController.java
+++ src/main/java/gateways/ch/controllers/GatewayController.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gateways.ch.controllers;
 
 import gateways.ch.models.Device;
 import gateways.ch.models.Gateway;
 import gateways.ch.service.DeviceServiceImpl;
+import gateways.ch.service.GatewayService;
 import gateways.ch.service.GatewayServiceImpl;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
